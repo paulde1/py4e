9dd35388db9e12ee538f5dda9a83292818e31929
Paul De Cunha <pauldecunha7117@gmail.com>
2023-03-06 18:26:06 -0500

    for-loop

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 print('hello Paul')
+
 x=11
 if x > 15:
     print('larger')
@@ -10,3 +11,22 @@ while n > 0:
     print(n)
     n = n -1
 print('gottaBlast')
+
+name = input('Enter File:')
+handle = open(name)
+
+counts = dict()
+for line in handle:
+    words = line.split()
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+
+largeCount = None
+largeWord = None
+
+for word, count in counts.items():
+    if largeCount is None or count > largeCount:
+        largeWord = word
+        largeCount  = count
+
+print(largeWord,largeCount)
