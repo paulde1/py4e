df4d26e0d4821de1a7ec84a2a2cadb497fc97024
Paul De Cunha <pauldecunha7117@gmail.com>
2023-03-06 18:46:36 -0500

    descriptive code

--- hello.py
+++ hello.py
@@ -1,20 +1,22 @@
+#print
 print('hello Paul')
-
+#considtional
 x=11
 if x > 15:
     print('larger')
 if x < 15:
     print('smaller')
-
+#while loop
 n= 5
 while n > 0:
     print(n)
     n = n -1
 print('gottaBlast')
-
+#for loop, get the name of the file and open it 
 name = input('Enter File:')
-handle = open(name)
+handle = open(name, 'r')
 
+#count word frequency
 counts = dict()
 for line in handle:
     words = line.split()
@@ -23,10 +25,10 @@ for line in handle:
 
 largeCount = None
 largeWord = None
-
+#locate the most common word
 for word, count in counts.items():
     if largeCount is None or count > largeCount:
         largeWord = word
         largeCount  = count
-
+#print results
 print(largeWord,largeCount)
