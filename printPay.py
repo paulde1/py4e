effea2efaff9992783b33d12e7ad8ec9571402cd
Paul De Cunha <pauldecunha7117@gmail.com>
2023-03-07 18:11:02 -0500

    overtime pay

--- printPay.py
+++ printPay.py
@@ -2,3 +2,16 @@ hrs = input("Enter Hours:")
 rate = input("Enter Rate:")
 pay = float(hrs) * float(rate)
 print("Pay:", pay)
+
+#overtime pay calculator
+hrs = input("Enter Hours:")
+rate = input("Enter Rate:")
+hrs = float(hrs)
+rate = float(rate)
+if hrs > 40 :
+    overtime = (hrs % 10) * 15.75
+    pay = (40 * rate) + overtime
+else : 
+    pay = hrs * rate
+    
+print(pay)
