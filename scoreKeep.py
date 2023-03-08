4d578e442b0775edcaaddebe8ad91e26b3edbdae
Paul De Cunha <pauldecunha7117@gmail.com>
2023-03-07 19:26:11 -0500

    working grade score

--- scoreKeep.py
+++ scoreKeep.py
@@ -1,22 +1,25 @@
-score = input('Enter Score:')
+def scoreKeep():
+    score = input('Enter Score:')
 
-grade = 'Score out of range'
-try:
-    score = float(score)
-    if score >= 0.0 and score <= 1.0:
-        if(score >= 0.9):
-            grade = 'A'
-        elif(score >= 0.8):
-            grade = 'B'
-        elif(score >= 0.7):
-            grade = 'C'
-        elif(score >= 0.6):
-            grade = 'D'
-        else: grade = 'F'
-    print(grade)
-except:
-    print ("error: invalid score type")
-    quit()
+    grade = 'Score out of range'
+    try:
+        score = float(score)
+        if score >= 0.0 and score <= 1.0:
+            if(score >= 0.9):
+                grade = 'A'
+            elif(score >= 0.8):
+                grade = 'B'
+            elif(score >= 0.7):
+                grade = 'C'
+            elif(score >= 0.6):
+                grade = 'D'
+            else: grade = 'F'
+        print(grade)
+    except:
+        print ("error: invalid score type")
+        quit()
+    
+scoreKeep()
 
 
   
