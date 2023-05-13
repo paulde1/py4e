65214c5f0763ee5c66ca84c5c83acce59d315ef4
Paul De Cunha <pauldecunha7117@gmail.com>
2023-05-13 19:06:22 -0400

    extra line lstrip bug fix in progress

--- class1/readfile.py
+++ class1/readfile.py
@@ -1,2 +1,13 @@
-fname = input("Enter file name: ")
-fh = open(fname)
+filename = input("Enter file name: ")
+stringbuilder = ''
+try:
+    filehandler = open(filename)
+except:
+    print("Error opening ", filename)
+    quit()
+
+for line in filehandler:
+    line = line.upper().lstrip()
+    stringbuilder += line
+print(stringbuilder)
+
