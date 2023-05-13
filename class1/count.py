e711a8c6f09bfe8fd5aac4ca10a85ecd6742e45c
Paul De Cunha <pauldecunha7117@gmail.com>
2023-05-13 18:44:20 -0400

    select and read file

---
+++ class1/count.py
@@ -0,0 +1,19 @@
+filehandler = open(filename)
+count = 0
+for line in filehandler: 
+    count += 1
+print('Total line count:', count)
+
+for line in filehandler:
+    if line.startswith('From:') :
+        print(line)
+
+## open file handler
+
+filename = input('Enter filename: ')
+filehandler = open(filename)
+count = 0
+for line in filehandler:
+    if line.startswith('subject:') :
+        count += 1
+print('There were ', count, ' subject lines in ', filename)
