eca0ad7f53bce27edda59efd49b1e8f2a9a56195
paulde1 <pauldecunha7117@gmail.com>
2023-05-24 17:44:13 -0400

    friends

---
+++ class1/friends.py
@@ -0,0 +1,9 @@
+friends = ['Joseph', 'Glenn', 'Sally']
+for freind in friends : 
+    print('Happy New Year:', freind)
+print('Done')
+
+z =  [ 'Joseph', 'Glenn', 'Sally']
+for x in z :
+    print('Happy New Year:', x)
+print('Done')
