37ac424f8688b26ce79fc1859c864dbbdff79751
paulde1 <pauldecunha7117@gmail.com>
2023-05-26 23:51:42 -0400

    bug fix in progress

--- class1/list.py
+++ class1/list.py
@@ -5,7 +5,10 @@ lst = list()
 for line in fh:
     line = line.rstrip()
     line = line.split(" ")
-    lst.append(line)
+
+print(line)
+    # for word in line:
+    #     lst.append(word)
 
 lst.sort()
 print(lst)
