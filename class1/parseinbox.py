41d5e70777f0e477af84919093ec3547c53f6860
Paul De Cunha <pauldecunha7117@gmail.com>
2023-05-30 11:47:38 -0400

    finsihed message parser

--- class1/parseinbox.py
+++ class1/parseinbox.py
@@ -5,4 +5,12 @@ if len(fname) < 1:
 fh = open(fname)
 count = 0
 
+for line in fh:
+    message = line.rstrip().split()
+    if "From" in message:
+        print(message[1])
+        count+=1
+    else:
+        continue
+
 print("There were", count, "lines in the file with From as the first word")
