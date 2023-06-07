d30eebbbac44948209be696f6a8884e6d6c9b36a
Paul De Cunha <pauldecunha7117@gmail.com>
2023-06-06 21:09:35 -0400

    fixing bug in dictionary

--- class1/purse.py
+++ class1/purse.py
@@ -2,6 +2,6 @@ purse = dict()
 purse['money']= 12
 purse['candy'] = 3
 purse['tissues'] = 75
-print(purse)
-purse['candy', "purse1"] = purse['candy'] + 2
+print(purse, "purse1")
+purse['candy'] = purse['candy'] + 2
 print(purse, "purse2")
