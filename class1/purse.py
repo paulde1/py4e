07e10e78d7049d7b4629c7ab5153b755feeb5e8d
Paul De Cunha <pauldecunha7117@gmail.com>
2023-06-06 21:16:49 -0400

    counting names

--- class1/purse.py
+++ class1/purse.py
@@ -2,6 +2,16 @@ purse = dict()
 purse['money']= 12
 purse['candy'] = 3
 purse['tissues'] = 75
-print(purse, "purse1")
+# print(purse, "purse1")
 purse['candy'] = purse['candy'] + 2
-print(purse, "purse2")
+# print(purse, "purse2")
+
+ 
+counts = dict()
+names = ['csev', 'cwen', 'csev', 'zqian', 'cwen']
+for name in names :
+    if name not in names :
+        counts[name] = 1
+    else :
+        counts[name]= counts[name] + 1
+print(counts)
