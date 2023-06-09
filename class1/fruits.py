a6ade9fe9d99502b4f0163f8e96c9ee8d9bf2ce3
Paul De Cunha <pauldecunha7117@gmail.com>
2023-06-08 22:50:03 -0400

    find length

--- class1/fruits.py
+++ class1/fruits.py
@@ -2,7 +2,7 @@ fruit = "banana"
 x = fruit.lower()
 print(x)
 lotto = [2,14,26,41,63]
-# print(range(lotto)) #integer
+print(range(lotto)) #integer
 print(range((len(lotto))))
 stuff = list()
 stuff.append('book')
