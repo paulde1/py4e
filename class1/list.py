881509875426a1f01f3d3bdd3a5885184418e2e5
Paul De Cunha <pauldecunha7117@gmail.com>
2023-06-08 22:52:02 -0400

    find name of length

--- class1/list.py
+++ class1/list.py
@@ -1,9 +1,9 @@
-fname = input("Enter file name: ")
-fh = open(fname)
+filename = input("Enter file name: ")
+filehandler = open(filename)
 lst = list()
 
-for line in fh:
-    for line in fh:                    
+for line in filehandler:
+    for line in filehandler:                    
         word= line.rstrip().split() 
         for element in word:        
             if element in lst:        
