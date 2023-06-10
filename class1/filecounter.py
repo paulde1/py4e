fdc1f28928d2b0bbeae2322dce24f77ca1ec6fff
Paul De Cunha <pauldecunha7117@gmail.com>
2023-06-10 13:35:43 -0400

    file word big counter

--- class1/filecounter.py
+++ class1/filecounter.py
@@ -1,3 +1,17 @@
 name = input("Enter File:")
 filehandler = open(name)
 
+counts = dict()
+for line in filehandler:
+    words = line.split()
+    for word in words:
+        counts[word]= counts.get(word, 0) + 1
+
+bigCounter = None
+bigWord = None
+for word,count in counts.items():
+    if bigCounter is None or count > bigCounter :
+        bigWord = word
+        bigCounter = count
+
+print (bigCounter, bigWord)
