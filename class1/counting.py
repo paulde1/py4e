a98bb4581f062d6f9ec3b230353664121503f478
Paul De Cunha <pauldecunha7117@gmail.com>
2023-06-10 13:04:12 -0400

    word couner list properties

--- class1/counting.py
+++ class1/counting.py
@@ -4,9 +4,21 @@ line = input('')
 
 words = line.split()
 
-print('Words:', words)
+# print('Words:', words)
 
 print('Counting ...')
 for word in words:
     counts[word] = counts.get(word, 0) + 1
 print('Counts', counts)
+
+ppp = {
+    "tommy": 1, 
+    "Angel" : 2, 
+    "chaya" : 3,
+    "Cidney" : 4, 
+    "ben" : 5
+}
+
+print(list(ppp))
+print(ppp.values())
+print(ppp.keys())
