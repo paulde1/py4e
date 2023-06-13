2ff6682687eb458369d99d33f88f177e669f3d59
Paul De Cunha <pauldecunha7117@gmail.com>
2023-06-13 12:59:24 -0400

    append list items

--- class1/quiz9.py
+++ class1/quiz9.py
@@ -1,5 +1,11 @@
 filename = input("Enter file: ")
 if len(filename) < 1 : name = "mboxshort.txt"
+handler = open(filename)
 
 list = list()
+
+for line in handler :
+    if not line.startswith( "From:"): continue
+    line = line.split()
+    list.append(line[1])
 
