f8a563c74cc2f9780425cd13d886c5897a46de00
Paul De Cunha <pauldecunha7117@gmail.com>
2023-06-15 18:02:34 -0400

    count value using tuple

---
+++ sortingfile.py
@@ -0,0 +1,16 @@
+filehandler = open("romeo.txt")
+counts = dict()
+for line in filehandler :
+    words = line.split()
+    for word in line :
+        counts[word] = counts.get(word,0) + 1
+
+list = list()
+for key, value in counts.items() :
+    newtuple = (value,key)
+    list.append(newtuple)
+
+list = sorted(list, reverse = True)
+
+for value, key in list[:10] :
+    print(key, value)
