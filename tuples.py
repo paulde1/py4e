1a568f88ccd5683f0c14224c325543b0b951b2ad
Paul De Cunha <pauldecunha7117@gmail.com>
2023-06-15 17:32:43 -0400

    tuples rules

--- tuples.py
+++ tuples.py
@@ -1,1 +1,10 @@
 # tuples are not mutables just like strings
+
+x = (3,5,4)
+x.sort() # leads to a traceback no .functions because sets it apart from list. 
+
+list = list()
+dir(list) # see all the dot functions available 
+
+tuple = tuple()
+dir(tuple) # only count and index available
